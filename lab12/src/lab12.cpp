b32521f498ac4b20e36e4581e147f9dca80f4c7b
nehabhardwaj <nehabhardwaj95@gmail.com>
2016-02-22 14:25:02 +0530

    fslab pgm-2

--- lab12/src/lab12.cpp
+++ lab12/src/lab12.cpp
@@ -5,11 +5,66 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
+//============================================================================
+// Name        : programs.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
 
 #include <iostream>
+#include<fstream>
 using namespace std;
+class student
+	{
+	private:
+		string name,usn,sem;
+	public:
+		void read();
+		void pack(char * xyz);
+		void write();
+	};
+void student::read()
+{
+	cout<<"enter name";
+	cin>> name;
+	cout<<"enter usn";
+	cin>>usn;
+	cout<<"enter sem";
+	cin>>sem;
+}
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+void student::pack(char * fname)
+{
+	 fstream fp;
+	 string buffer;
+	 buffer=name+'|'+usn+'|'+sem;
+	 buffer.resize(100,'*');
+	 fp.open(fname,ios::out);
+	 fp<<buffer;
+	 fp.close();
+}
+int main()
+{
+	student s;
+	char * abc;
+	int ch;
+	while(1)
+	{
+	cout<<"1.insert\t 2.search\t 3.delete\t 4.modification\t 5.exit\t";
+	cout<<"enter your choice";
+	cin>>ch;
+	switch(ch)
+	{
+	case 1: cout<< "enter filename";
+	cin>> abc;
+	s.read();
+	s.pack(abc);
+	break;
+	case 5: cout<<"wrong input";
+	break;
+}
+	}
 	return 0;
 }
